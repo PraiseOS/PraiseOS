e23fad7f8bd2bf6f71ec17d4602de99b7c409818
Physicio <otto.fiedler@outlook.com>
2022-12-17 07:24:31 +1000

    Updated Installation Files

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 @echo off
 
 py -m pip install -r requirements.txt
+py -m pip install windows-curses
 py init.py
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 pip3 install -r requirements.txt
+py -m pip install curses
 python3 init.py
